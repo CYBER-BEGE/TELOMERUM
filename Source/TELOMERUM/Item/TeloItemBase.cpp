eae62ef57deb8332171eb3baf6a52f74caee08a0
Arkru <35932023+Arkru1204@users.noreply.github.com>
2026-03-24 23:24:13 +0900

    [Chore] Add source folder for item/trap

--- Source/TELOMERUM/Item/TeloItemBase.cpp
+++ Source/TELOMERUM/Item/TeloItemBase.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "TeloItemBase.h"
+#include "Item/TeloItemBase.h"
 
 // Sets default values
 ATeloItemBase::ATeloItemBase()
